528c7249d597ef1823531869fdb2ce039f43fcb9
Harsh Upadhyay <uharsh328@gmail.com>
2026-01-19 23:01:23 +0530

    Add Analytics component and update dependencies for tracking

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Education from "@/components/Education";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <Experience />
       <Projects />
       <Footer />
+      <Analytics />
     </main>
   );
 }
